297dadebc2e96c3bcab9f626181f407f420c7e00
Alexandra <sandra.larrosa88@gmail.com>
2020-07-04 12:12:16 +0200

    add function add Favourite

--- app/js/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const ENDPOINT = 'https://beta.adalab.es/ejercicios-extra/js-fetch-arrays-princesas-disney/data/users.json'
-
-let users = [];
-let favorites = [];
-
-/* Do your magic! 🦄🦄🦄*/
-
--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ const dataUsers = '../data/users.json';
 const listHTML = document.querySelector('.js-user-list');
 
 let users = [];
-let favorites = [];
 
 /* Do your magic! 🦄🦄🦄*/
 
@@ -17,10 +16,40 @@ function conectToData() {
     });
 }
 
-conectToData();
-
 function paintUsers(arr) {
-  for (let user of arr) {
-    listHTML.innerHTML += `<li class="content__list"><div class="card"><div class ="img"><img class="foto" src="${user.picture}"></div><h3 class="name">${user.name}</h3><p class="text">${user.comment}</p><i class="far fa-heart heart"></i></div></li>`;
+  for (let i = 0; i < arr.length; i++) {
+    const user = users[i];
+    listHTML.innerHTML += `<li class="content__list"><div id="princess${i}" class="card"><div class ="img"><img class="foto" src="${user.picture}"></div><h3 class="name">${user.name}</h3><p class="text">${user.comment}</p><i id="heart${i}" class="far fa-heart heart"></i></div></li>`;
+  }
+
+  listenerHeart();
+}
+
+/* Listen Heart */
+function listenerHeart() {
+  const hearts = document.querySelectorAll('.heart');
+  for (const heart of hearts) {
+    heart.addEventListener('click', addFavourite);
   }
 }
+
+let favorites = [];
+
+function addFavourite(ev) {
+  const heartUser = ev.currentTarget;
+  console.log(heartUser);
+  const heartParent = heartUser.parentElement;
+  console.log(heartParent);
+  if (favorites.indexOf(heartParent) === -1) {
+    favorites.push(heartParent);
+    heartUser.classList.remove('heart');
+    heartUser.classList.add('iconBold');
+    heartParent.classList.add('backgroundColor');
+  } else {
+    favorites.reverse(heartParent);
+    heartParent.classList.remove('backgroundColor');
+  }
+  console.log(favorites);
+}
+
+conectToData();
